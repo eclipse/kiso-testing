c941738fb821324ed64745772297d3b17550ae72
mrlang <Lang.uw@googlemail.com>
2022-02-15 09:16:29 +0100

    docs(gen_changelog): change starting version for changelog

--- gen_changelog.sh
+++ gen_changelog.sh
@@ -1,7 +1,7 @@
 
 if which npx > /dev/null 2>&1
 then
-    npx auto-changelog -p --sort-commits date --starting-version 0.15.0
+    npx auto-changelog -p --sort-commits date --starting-version 0.0.0-alpha
 else
     echo ERROR: Update changelog failed.
     echo npx not available on your system. Install nodejs -> https://nodejs.org/en/download/
