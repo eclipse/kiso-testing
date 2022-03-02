c135887025563d6ac58b12f1ff9a89e149d6a3c6
Sébastian Clerson <s.clerson@technologyandstrategy.com>
2022-03-02 14:05:19 +0100

    Investigate ScalarNode attributes

--- src/pykiso/config_parser.py
+++ src/pykiso/config_parser.py
@@ -20,28 +20,34 @@ Configuration File Parser
 
 """
 
-import ast
-import io
 import logging
+import operator
 import os
 import re
 import sys
-import typing
 from distutils.version import LooseVersion
 from pathlib import Path
-from typing import Callable, TextIO, Union
+from typing import Callable, Dict, List, TextIO, Union
 
 import pkg_resources
 import yaml
 
-from .types import PathType
+from pykiso.types import PathType
 
 
-def check_requirements(requirements: list):
+def check_requirements(requirements: List[dict]):
     """Check the environment before running the tests
 
     :param requirements: requirements to be checked
     """
+    condition_to_operator = {
+        "<": operator.lt,
+        "<=": operator.le,
+        ">": operator.gt,
+        ">=": operator.ge,
+        "==": operator.eq,
+        "!=": operator.ne,
+    }
     requirement_satisfied = True
     for req in requirements:
         for package, expected_version in req.items():
@@ -57,31 +63,21 @@ def check_requirements(requirements: list):
                     # pattern found eg: match = ['', '>=', '0.1.2']
                     condition = match[1]
                     exp_version = match[2].strip()
-                    check = (
-                        LooseVersion(current_version) < LooseVersion(exp_version)
-                        if condition == "<"
-                        else LooseVersion(current_version) <= LooseVersion(exp_version)
-                        if condition == "<="
-                        else LooseVersion(current_version) > LooseVersion(exp_version)
-                        if condition == ">"
-                        else LooseVersion(current_version) >= LooseVersion(exp_version)
-                        if condition == ">="
-                        else LooseVersion(current_version) == LooseVersion(exp_version)
-                        if condition == "=="
-                        else LooseVersion(current_version) != LooseVersion(exp_version)
-                        if condition == "!="
-                        else None
-                    )
 
-                    if check is False:
-                        # Version not satisfied
-                        logging.error(
-                            f"Requirement issue: found {package} with version '{current_version}' but '{expected_version}' given"
+                    try:
+                        compare_operation = condition_to_operator[condition]
+                        check = compare_operation(
+                            LooseVersion(current_version), LooseVersion(exp_version)
                         )
-                    elif check is None:
+                        if check is False:
+                            # Version not satisfied
+                            logging.error(
+                                f"Requirement issue: found {package} with version '{current_version}' but '{expected_version}' given"
+                            )
+                    except KeyError as e:
                         # comparator invalid
                         logging.error(
-                            f"Requirement issue: comparator '{condition}' not among [<, <=, >, >=, ==, !=]"
+                            f"Requirement issue: comparator '{e}' not among {list(condition_to_operator.keys())}"
                         )
                         check = False
 
@@ -105,178 +101,7 @@ def check_requirements(requirements: list):
         sys.exit(1)
 
 
-def parse_config(fname: PathType) -> typing.Dict:
-    """Parse YAML config file.
-
-    :param fname: path to the config file
-
-    :return: config dict with resolved paths where needed
-    """
-
-    class YamlLoader(yaml.SafeLoader):
-        """Extension of default yaml.SafeLoader who integrates
-        custom !include tag management.
-        """
-
-        def __init__(self, stream: io.TextIOWrapper):
-            """Initialize attributes.
-
-            :param stream: current stream in use
-            """
-            self._root = Path(fname).resolve().parent
-            super().__init__(stream)
-
-        def include(self, node: yaml.nodes.ScalarNode) -> dict:
-            """Return the content of an yaml file identify by !include
-            tag.
-
-            :param node: ScalarNode currently in use
-
-            :return: yaml file content
-            """
-            filename = (self._root / Path(node.value)).resolve()
-            with open(filename, "r") as f:
-                return yaml.load(f, Loader=YamlLoader)
-
-    YamlLoader.add_constructor("!include", YamlLoader.include)
-
-    with open(fname) as f:
-        cfg = yaml.load(f.read(), Loader=YamlLoader)
-
-    # fix suite-dirs
-    base_dir = Path(fname).resolve().parent
-
-    def _fix_types_loc(thing: dict) -> dict:
-        """Parse the type field in the config file and resolves it if necessary.
-
-        :param thing: config sub-dict containing the type
-
-        :return: config sub-dict containing the resolved type if needed
-        """
-        loc, _class = thing["type"].rsplit(":", 1)
-        if not loc.endswith(".py"):
-            return thing
-        path = Path(loc)
-        if not path.is_absolute():
-            path = (base_dir / path).resolve()
-            thing["type"] = str(path) + ":" + _class
-            logging.debug(f'Resolved path : {thing["type"]}')
-        return thing
-
-    def _parse_env_var(config_element: str) -> str:
-        """Search for an environment variable pattern in the yaml file and
-        (1) try to replace it with the value of an environment variable of the same name
-        (2) Assign the default value if the environment variable was not found
-        (3) raise an error if 1. and 2. failed
-
-        Additionally, cast the value if it matches an integer.
-
-        :param config_element: config sub-dict value
-
-        :return: config sub-dict value with replaced environment variable if needed
-
-        :raise: ValueError if the environment variable not found and no default value specified
-        """
-        # Check for regular expression "ENV{word=.}"
-        match = re.compile(r"ENV{(\w+)(=(.+))?}").findall(str(config_element))
-        if match:
-            # Parse detected environment variable
-            match_single_env = str(match[0][0])
-            match_env_with_val = str(match[0][2])
-
-            if match_single_env in os.environ:
-                env = os.environ[match_single_env]
-            elif match_env_with_val:
-                env = match_env_with_val
-            else:
-                raise ValueError(
-                    f"Environment variable {match_single_env} not found and no default value specified"
-                )
-            is_numeric = re.fullmatch(r"\d+", env)
-            is_hex = re.fullmatch(r"0x[0-9a-fA-F]+", env)
-            is_bool = env.lower() in ["true", "false"]
-            if is_numeric is not None:
-                config_element = int(env)
-            elif is_hex is not None:
-                config_element = int(env, base=16)
-            elif is_bool:
-                config_element = env.lower() == "true"
-            else:
-                config_element = env
-            logging.debug(
-                f"Replaced environment variable {match_single_env} with {config_element}"
-            )
-        return config_element
-
-    def _resolve_path(config_path: str) -> str:
-        """Resolve a path relative to the config file's location.
-
-        :param config_path: a config path
-
-        :return: the resolved config path if needed
-        """
-        config_path_unresolved = Path(config_path)
-        if not config_path_unresolved.is_absolute():
-            config_path = (base_dir / config_path_unresolved).resolve()
-            logging.debug(
-                f"Resolved relative path {config_path_unresolved} to {config_path}"
-            )
-        return str(config_path)
-
-    def _check_path(path: str) -> bool:
-        """Check if the argument is a valid path.
-
-        :param path: any string in the config file's values
-
-        :return: a boolean indicating if the string is a valid path
-        """
-        is_valid = False
-        if "./" in path.replace(".\\", "./"):
-            invalid_characters = (":", "*", "?", "<", ">", "|")
-            is_valid = (
-                False
-                if (any(c in invalid_characters for c in path))
-                else (Path(path).exists() or (base_dir / Path(path)).exists())
-            )
-        return is_valid
-
-    def _resolve_config_paths(config: dict) -> dict:
-        """Iterate over the config dict and resolve all of the config file paths.
-
-        :param config: config dict
-
-        :return: config dict with resolved paths
-        """
-        for key in config.keys():
-            if isinstance(config.get(key), dict):
-                _resolve_config_paths(config.get(key))
-            elif isinstance(config.get(key), str):
-                config[key] = _parse_env_var(config[key])
-                config[key] = (
-                    _resolve_path(config[key])
-                    if isinstance(config[key], str) and _check_path(config[key])
-                    else config[key]
-                )
-            elif isinstance(config.get(key), list) and key == "test_suite_list":
-                for test_suite in config[key]:
-                    test_suite["suite_dir"] = _resolve_path(
-                        _parse_env_var(test_suite["suite_dir"])
-                    )
-        return config
-
-    cfg["connectors"] = {n: _fix_types_loc(s) for n, s in cfg["connectors"].items()}
-    cfg["auxiliaries"] = {n: _fix_types_loc(s) for n, s in cfg["auxiliaries"].items()}
-    cfg = _resolve_config_paths(cfg)
-
-    # Check requirements
-    requirements = cfg.get("requirements")
-    if requirements:
-        check_requirements(requirements)
-
-    return cfg
-
-
-def _parse_config(fname: PathType) -> typing.Dict:
+def parse_config(fname: PathType) -> Dict:
     """Parse YAML config file.
 
     :param fname: path to the config file
@@ -291,7 +116,7 @@ def _parse_config(fname: PathType) -> typing.Dict:
         """
 
         type_pattern = re.compile(r".*:.*")
-        env_var_pattern = re.compile(r"ENV{(\w+)}")
+        env_var_pattern = re.compile(r"ENV{(\w+)(=(.+))?}")
         rel_path_pattern = re.compile(r'[^\n"?:*<>|]')
 
         def __init__(self, stream: TextIO):
@@ -315,17 +140,42 @@ def _parse_config(fname: PathType) -> typing.Dict:
             YamlLoader.add_implicit_resolver(tag, pattern, first=None)
             YamlLoader.add_constructor(tag, constructor)
 
+        @staticmethod
+        def is_key(node: yaml.nodes.ScalarNode) -> bool:
+            print()
+            print("Value", node.value)
+            print("Tag", node.tag)
+            end_mark: yaml.error.Mark = node.end_mark
+            if end_mark.buffer is None:
+                print("\n    buffer", node.end_mark.buffer)
+                print("\n    column", node.end_mark.column)
+                print("\n    index", node.end_mark.index)
+                print("\n    line", node.end_mark.line)
+                print("\n    name", node.end_mark.name)
+                print("\n    pointer", node.end_mark.pointer)
+                return True
+            print("Test start", node.start_mark.buffer[node.start_mark.pointer])
+            print("Test end", node.end_mark.buffer[node.end_mark.pointer])
+            return node.end_mark.buffer[node.end_mark.pointer] == ":"
+
+
         def include(self, node: yaml.nodes.ScalarNode) -> dict:
-            """Return the content of an yaml file identify by !include
+            """Return the content of a yaml file identified by !include
             tag.
 
             :param node: ScalarNode currently in use
 
             :return: yaml file content
             """
-            filename = self._root / Path(node.value).resolve()
-            with open(filename, "r") as f:
-                return yaml.load(f, Loader=YamlLoader)
+            print("*************** INCLUDE")
+            print("\n    buffer", node.end_mark.buffer)
+            print("\n    column", node.end_mark.column)
+            print("\n    index", node.end_mark.index)
+            print("\n    line", node.end_mark.line)
+            print("\n    name", node.end_mark.name)
+            print("\n    pointer", node.end_mark.pointer)
+            filename = (self._root / Path(node.value)).resolve()
+            return yaml.load(filename.read_text(), Loader=YamlLoader)
 
         def resolve_path(self, node: yaml.nodes.ScalarNode) -> str:
             """Resolve a path relative to the config file's location.
@@ -334,6 +184,23 @@ def _parse_config(fname: PathType) -> typing.Dict:
 
             :return: the resolved config path if needed
             """
+
+            """
+            print("Start mark", node.start_mark)
+            print("End mark")
+            print("\n    buffer", node.end_mark.buffer)
+            print("\n    column", node.end_mark.column)
+            print("\n    index", node.end_mark.index)
+            print("\n    line", node.end_mark.line)
+            print("\n    name", node.end_mark.name)
+            print("\n    pointer", node.end_mark.pointer)
+            """
+
+            if YamlLoader.is_key(node):
+                return str(node.value)
+
+            # print("ID", node.id)
+            # print("Style", node.style)
             value = node.value
             config_path_unresolved = Path(value)
             if not config_path_unresolved.is_absolute():
@@ -364,29 +231,53 @@ def _parse_config(fname: PathType) -> typing.Dict:
             return thing
 
         def parse_env_var(self, node: yaml.nodes.ScalarNode) -> Union[str, int]:
-            """Search for an environment variable and replace it with the associated value.
+            """Search for an environment variable pattern in the yaml file and
+            (1) try to replace it with the value of an environment variable of the same name
+            (2) Assign the default value if the environment variable was not found
+            (3) raise an error if 1. and 2. failed
 
-            Additionally, cast the value if it matches a python type supported by ast.literal_eval().
+            Additionally, cast the value if it matches an integer.
 
             :param node: ScalarNode currently in use
 
             :return: config sub-dict value with replaced environment variable if needed
+
+            :raise: ValueError if the environment variable not found and no default value specified
             """
-            match = re.search(self.env_var_pattern, node.value)
+            if node.tag == self.DEFAULT_SCALAR_TAG:
+                logging.warning(
+                    "YAML warning: environment variables should not be put in quotes"
+                )
+            match = re.findall(self.env_var_pattern, node.value)
             # no match means the node is just a string
-            if match is None:
+            if not match:
                 return str(node.value)
-            env_name = match.group(1)
-            env = os.environ[env_name]
-            # cast the env var's value
-            try:
-                config_element = ast.literal_eval(env)
-            except Exception:
+            # Parse detected environment variable
+            match_single_env = str(match[0][0])
+            match_env_with_val = str(match[0][2])
+
+            if match_single_env in os.environ:
+                env = os.environ[match_single_env]
+            elif match_env_with_val:
+                env = match_env_with_val
+            else:
+                raise ValueError(
+                    f"Environment variable {match_single_env} not found and no default value specified"
+                )
+            is_numeric = re.fullmatch(r"\d+", env)
+            is_hex = re.fullmatch(r"0x[0-9a-fA-F]+", env)
+            is_bool = env.lower() in ["true", "false"]
+            if is_numeric is not None:
+                config_element = int(env)
+            elif is_hex is not None:
+                config_element = int(env, base=16)
+            elif is_bool:
+                config_element = env.lower() == "true"
+            else:
                 config_element = env
             logging.debug(
-                f"Replaced environment variable {env_name} with {config_element}"
+                f"Replaced environment variable {match_single_env} with {config_element}"
             )
-            return config_element
 
     YamlLoader.add_constructor("!include", YamlLoader.include)
     # parse quoted environment variables
@@ -402,7 +293,20 @@ def _parse_config(fname: PathType) -> typing.Dict:
         "!resolve", YamlLoader.rel_path_pattern, YamlLoader.resolve_path
     )
 
-    with open(fname) as f:
-        cfg = yaml.load(f.read(), Loader=YamlLoader)
+    cfg = yaml.load(f.read_text(), Loader=YamlLoader)
+
+    # Check requirements
+    requirements = cfg.get("requirements")
+    if requirements:
+        check_requirements(requirements)
 
     return cfg
+
+
+if __name__ == "__main__":
+    import json
+
+    os.environ["TEST_SUITE_1"] = "test_suite_1"
+    f = Path(__file__).resolve().parent / "nested_yaml/test.yaml"
+
+    print(json.dumps(parse_config(f), indent=2))
