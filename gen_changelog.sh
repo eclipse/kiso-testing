ae3893ae34d608f592f28b57dbb159f10b73e388
Uwe Lang <lang.uw@googlemail.com>
2022-03-03 13:13:55 +0100

    docs: Change auto changelog tool

--- gen_changelog.sh
+++
@@ -1,8 +0,0 @@
-
-if which npx > /dev/null 2>&1
-then
-    npx auto-changelog -p --sort-commits date --starting-version 0.15.0
-else
-    echo ERROR: Update changelog failed.
-    echo npx not available on your system. Install nodejs -> https://nodejs.org/en/download/
-fi
