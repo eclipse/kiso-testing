a7afddbf191a2c01d4e2031f80b0ce4ade6f0efd
Teddy Riolet <external.teddy.riolet@de.bosch.com>
2022-07-12 17:40:37 +0200

    feat: add robot framework for UDS tester present sender
    bugfix: change space into tab in robot demo

--- src/pykiso/lib/robot_framework/uds_auxiliary.py
+++ src/pykiso/lib/robot_framework/uds_auxiliary.py
@@ -20,6 +20,7 @@ UDS Auxiliary plugin
 
 """
 
+import threading
 from typing import List, Optional, Union
 
 from robot.api import logger
@@ -41,6 +42,8 @@ class UdsAuxiliary(RobotAuxInterface):
     def __init__(self):
         """Initialize attributes."""
         super().__init__(aux_type=UdsAux)
+        self.sender_stop_event = threading.Event()
+        self.sender = None
 
     @keyword(name="Send uds raw")
     def send_uds_raw(
@@ -161,6 +164,28 @@ class UdsAuxiliary(RobotAuxInterface):
         """
         aux = self._get_aux(aux_alias)
         return aux.write_data(parameter, value)
+
+    @keyword(name="Start tester present sender")
+    def start_tester_present_sender(self, period: int, aux_alias) -> None:
+        """Start to continuously sends tester present messages via UDS
+
+        :param period: period in seconds to use for the cyclic sending of tester present
+        :param aux_alias: auxiliary's alias
+        """
+        aux = self._get_aux(aux_alias)
+        self.sender = threading.Thread(
+            name="TesterPresentSender",
+            target=aux._sender_run,
+            args=(period, self.sender_stop_event),
+        )
+        self.sender.start()
+
+    @keyword(name="Stop tester present sender")
+    def stop_tester_present_sender(self) -> None:
+        """Stop to continuously sends tester present messages via UDS"""
+        self.sender_stop_event.set()
+        self.sender.join()
+        self.sender_stop_event.clear()
 
     @staticmethod
     def get_service_id(service_name: str) -> int:
