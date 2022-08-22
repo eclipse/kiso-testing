a775db4b98a43bd9c43cb14d92caa291d3d47df6
Teddy Riolet <external.teddy.riolet@de.bosch.com>
2022-08-22 16:39:21 +0200

    feat: add unit test for addition of log levels

--- tests/test_auxiliary.py
+++ tests/test_auxiliary.py
@@ -7,8 +7,9 @@
 # SPDX-License-Identifier: EPL-2.0
 ##########################################################################
 
+from multiprocessing.connection import deliver_challenge
 import time
-
+import logging
 import pytest
 
 from pykiso import (
@@ -17,6 +18,7 @@ from pykiso import (
     SimpleAuxiliaryInterface,
     logging_initializer,
 )
+import pykiso
 from pykiso.auxiliary import AuxiliaryCommon
 from pykiso.exceptions import AuxiliaryCreationError
 from pykiso.test_setup.config_registry import ConfigRegistry
@@ -94,6 +96,43 @@ def mock_simple_aux(mocker):
         _delete_auxiliary_instance = mocker.stub(name="_delete_auxiliary_instance")
 
     return MockSimpleAux()
+
+
+def test_add_internal_log_levels():
+
+    # does the opposite of add_logging_level, so log levels addition can be tested
+    def del_logging_level(level_name):
+        method_name = level_name.lower()
+
+        delattr(logging, level_name)
+        delattr(logging.getLoggerClass(), method_name)
+        delattr(logging, method_name)
+
+    del_logging_level("INTERNAL_WARNING")
+    del_logging_level("INTERNAL_INFO")
+    del_logging_level("INTERNAL_DEBUG")
+
+    assert not hasattr(logging, "INTERNAL_INFO")
+    assert not hasattr(logging, "INTERNAL_DEBUG")
+    assert not hasattr(logging, "INTERNAL_WARNING")
+
+    class DummyAux(AuxiliaryCommon):
+        def __init__(self):
+            super().__init__()
+
+        def create_instance(self):
+            pass
+
+        def delete_instance(self):
+            pass
+
+        def run(self):
+            pass
+
+    my_dummy_aux = DummyAux()
+    assert hasattr(logging, "INTERNAL_INFO")
+    assert hasattr(logging, "INTERNAL_DEBUG")
+    assert hasattr(logging, "INTERNAL_WARNING")
 
 
 def test_thread_aux_raise_creation_error(mock_thread_aux):
