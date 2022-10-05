221fe0355f66c34e178c63aa56b4ede39e082d70
Sebastian Kotulla <sebastian.kotulla@de.bosch.com>
2022-10-05 14:02:53 +0200

    Fix problem with newlines on Linux

--- tests/test_cc_process.py
+++ tests/test_cc_process.py
@@ -88,7 +88,7 @@ def test_process_binary(mocker):
             # print "hello" on stdout
             # sleep 1s
             # print "pykiso" on stdout
-            'import sys;import time;print(sys.stdin.readline().strip());sys.stdout.flush();time.sleep(1);print(\'error\', file=sys.stderr);sys.stderr.flush();time.sleep(1);print("hello");sys.stdout.flush();time.sleep(1);print("pykiso")',
+            'import sys;import time;sys.stdout.write(sys.stdin.readline().strip());sys.stdout.flush();time.sleep(1);sys.stderr.write("error");sys.stderr.flush();time.sleep(1);sys.stdout.write("hello");sys.stdout.flush();time.sleep(1);sys.stdout.write("pykiso")',
         ],
     )
     # Start the process
@@ -96,21 +96,12 @@ def test_process_binary(mocker):
     # Second start raises an exception because the process is already running
     with pytest.raises(CCProcessError):
         cc_process.start()
-    """cc_process.cc_send(
-        {
-            "command": "start",
-            "executable": executable,
-            "args": [
-                "-c",
-                'import sys;import time;print(sys.stdin.readline().strip());sys.stdout.flush();time.sleep(1);print(\'error\', file=sys.stderr);sys.stderr.flush();time.sleep(1);print("hello");sys.stdout.flush();time.sleep(1);print("pykiso")',
-            ],
-        }
-    )"""
+
     cc_process._cc_send(b"hi\n")
-    assert cc_process.cc_receive(3) == {"msg": {"stdout": b"hi\r\n"}}
-    assert cc_process.cc_receive(3) == {"msg": {"stderr": b"error\r\n"}}
-    assert cc_process.cc_receive(3) == {"msg": {"stdout": b"hello\r\n"}}
-    assert cc_process.cc_receive(3) == {"msg": {"stdout": b"pykiso\r\n"}}
+    assert cc_process.cc_receive(3) == {"msg": {"stdout": b"hi"}}
+    assert cc_process.cc_receive(3) == {"msg": {"stderr": b"error"}}
+    assert cc_process.cc_receive(3) == {"msg": {"stdout": b"hello"}}
+    assert cc_process.cc_receive(3) == {"msg": {"stdout": b"pykiso"}}
     assert cc_process.cc_receive(3) == {"msg": {"exit": 0}}
     cc_process._cc_close()
     assert cc_process.cc_receive(3) == {"msg": None}
