5359ca0b481015068cb98a516d6002af4ff58d84
Yann poupon <y.poupon@technologyandstrategy.com>
2023-02-13 10:55:55 +0100

    tests: reworking cc_uart test

--- tests/test_cc_uart.py
+++ tests/test_cc_uart.py
@@ -1,5 +1,5 @@
 ##########################################################################
-# Copyright (c) 2010-2022 Robert Bosch GmbH
+# Copyright (c) 2010-2023 Robert Bosch GmbH
 # This program and the accompanying materials are made available under the
 # terms of the Eclipse Public License 2.0 which is available at
 # http://www.eclipse.org/legal/epl-2.0.
@@ -7,117 +7,101 @@
 # SPDX-License-Identifier: EPL-2.0
 ##########################################################################
 
-import pathlib
-import subprocess
-import sys
-import threading
-import time
 
 import pytest
 
 from pykiso import message
-from pykiso.lib.connectors.cc_uart import CCUart
+from pykiso.lib.connectors.cc_uart import CCUart, IncompleteCCMsgError
 
 
 @pytest.fixture
-def virtual_serial(tmp_path):
-    UART0 = tmp_path / "vt0"
-    UART1 = tmp_path / "vt1"
-    BAUD = 9600
-
-    uart0 = str(pathlib.Path(UART0).expanduser())
-    uart1 = str(pathlib.Path(UART1).expanduser())
-
-    print(f"running 'socat -d -d pty,link={uart0},rawer pty,link={uart1},rawer'")
-    socat = subprocess.Popen(
-        [
-            "/usr/bin/env",
-            "socat",
-            "-d",
-            "-d",
-            f"pty,link={uart0},rawer",
-            f"pty,link={uart1},rawer",
-        ],
-    )
-    time.sleep(1)  # wait for socat to be up and running
-    assert socat.returncode is None
-    yield uart0, uart1, BAUD
-    socat.terminate()
+def CCuart_instance():
+    return CCUart(serialPort="port")
 
 
-def _communication_send_check(serial_port, baud):
-    """Test to do with HW connected or UART emulator"""
-    # message_to_send = message.Message(msg_type=message.MessageType.COMMAND, sub_type=message.MessageCommandType.TEST_CASE_SETUP, test_section=1, test_suite=1, test_case=1)
-    message_to_send = message.Message(
-        msg_type=message.MessageType.ACK,
-        sub_type=message.MessageAckType.ACK,
-        test_suite=2,
-        test_case=3,
-    )
-    # manually set msg token so it can be checked for
-    message_to_send.msg_token = 0
-    # Initialize and open uart
-    print(f"sending test message to {serial_port}")
-    ch = CCUart(serialPort=serial_port, baudrate=baud)
-    ch.open()
-    # Send message
-    ch._cc_send(message_to_send)
-    # Close uart
-    ch.close()
+@pytest.fixture
+def serial_read_mock(mocker):
+    return mocker.patch("serial.Serial.read")
+
+
+def test_incomplete_ccmsg_error(caplog):
+    value = 12
+    msg_error = IncompleteCCMsgError(value)
+
+    assert str(msg_error) == "12"
+
+
+def test__cc_open(CCuart_instance, mocker):
+    open_serial_mock = mocker.patch("serial.Serial.open")
+
+    CCuart_instance._cc_open()
+
+    open_serial_mock.assert_called_once_with()
+
+
+def test__cc_close(CCuart_instance, mocker):
+    close_serial_mock = mocker.patch("serial.Serial.close")
+
+    CCuart_instance._cc_close()
+
+    close_serial_mock.assert_called_once_with()
+
+
+def test__cc_send(mocker, CCuart_instance):
+    msg = message.Message()
+    _send_using_slip_mock = mocker.patch.object(CCuart_instance, "_send_using_slip")
+
+    CCuart_instance._cc_send(msg)
+
+    _send_using_slip_mock.assert_called_once()
+
+
+def test__cc_receive_no_msg(CCuart_instance, serial_read_mock):
+    serial_read_mock.return_value = []
+
+    msg = CCuart_instance._cc_receive()
+
+    serial_read_mock.assert_called_once_with(1)
+    assert msg is None
+
+
+def test__cc_receive_not_expexted_crc32(CCuart_instance, serial_read_mock):
+    serial_read_mock.side_effect = [
+        (0xC0,),
+        (0xC0,),
+        (CCUart.ESC,),
+        (CCUart.ESC_START,),
+        (CCUart.ESC,),
+        (CCUart.ESC_ESC,),
+    ] + [(0x0,)] * 10
+
+    msg = CCuart_instance._cc_receive()
+
+    assert serial_read_mock.call_count == 14
+    assert msg is None
+
+
+def test__cc_receive_expexted_crc32(CCuart_instance, serial_read_mock):
+    serial_read_mock.side_effect = [(0xC0,), (0xC0,)] + [(0x0,)] * 10
+
+    msg = CCuart_instance._cc_receive()
+
+    assert serial_read_mock.call_count == 12
+    assert msg is not None
 
 
 @pytest.mark.parametrize(
-    "timeout, expected_timeout",
-    [
-        (5, 5),
-        (0, 1e-6),
-        (None, 1e-6),
-    ],
+    "raw_packet,bytes_expected,call_count",
+    [([0xC0], CCUart.ESC_START, 3), ([0xDB], CCUart.ESC_ESC, 3), ([0x12], 0x12, 2)],
 )
-def _communication_receive_check(timeout, expected_timeout, serial_port, baud):
-    """Test to do with HW connected or UART emulator"""
-    message_sent = message.Message(
-        msg_type=message.MessageType.COMMAND,
-        sub_type=message.MessageCommandType.TEST_CASE_SETUP,
-        test_suite=2,
-        test_case=3,
-    )
-    # manually set msg token so it matches the received token
-    message_sent.msg_token = 0
-    # Initialize and open uart
-    ch = CCUart(serialPort=serial_port, baudrate=baud)
-    ch.open()
-    # Wait for a message to be sent: it is C0 78 5E 60 01 00 00 01 02 03 00 C0
-    print(f"waiting to receive test message from {serial_port}")
-    ack_message_received = ch._cc_receive(timeout=timeout)
-    # manually set msg token so it matches the sent token
-    ack_message_received.msg_token = 0
-    print(f"received: {ack_message_received}")
-    # Check validity of the message
-    result = message_sent.check_if_ack_message_is_matching(ack_message_received)
-    print("Test 2 result: {}".format(result))
-    assert result
-    assert ch.serial.timeout == expected_timeout
-    # Close uart
-    ch.close()
+def test__send_using_slip(
+    CCuart_instance, mocker, raw_packet, bytes_expected, call_count
+):
+    write_mocker = mocker.patch("serial.Serial.write")
+    flush_output_mock = mocker.patch("serial.Serial.flushOutput")
 
+    CCuart_instance._send_using_slip(raw_packet)
 
-@pytest.mark.skip(
-    reason="This test is unreliable and runs on linux only. Will be refactored."
-)
-def test_virtual_communication(virtual_serial):
-    uart0, uart1, BAUD = virtual_serial
-    threads = [
-        threading.Thread(
-            target=_communication_receive_check,
-            kwargs=dict(serial_port=uart0, baud=BAUD),
-        ),
-        threading.Thread(
-            target=_communication_send_check,
-            kwargs=dict(serial_port=uart1, baud=BAUD),
-        ),
-    ]
-    for t in threads:
-        t.start()
-    for t in threads:
-        t.join(timeout=10)
+    assert write_mocker.call_count == call_count
+    write_mocker.assert_called_with(bytes_expected.to_bytes(1, byteorder="big"))
+    flush_output_mock.assert_called_once()
