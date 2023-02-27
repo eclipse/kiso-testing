896d5cdf7631f6a891fa3dcac9dbb1acb67ffb17
Yann poupon <100286656+yannpoupon@users.noreply.github.com>
2023-02-27 16:18:40 +0100

    tests: adding test for show_tag and pykiso to pytest (#244)
    * test: add tests for pykiso tools

---
+++ tests/test_pykiso_to_pytest.py
@@ -0,0 +1,244 @@
+##########################################################################
+# Copyright (c) 2010-2023 Robert Bosch GmbH
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0.
+#
+# SPDX-License-Identifier: EPL-2.0
+##########################################################################
+
+
+import pytest
+from click.testing import CliRunner
+
+from pykiso.tool.pykiso_to_pytest.cli import (
+    find_string_in,
+    format_value,
+    get_imports,
+    main,
+    nested_get,
+    remove_key,
+)
+
+
+@pytest.fixture
+def pykiso_config():
+    return {
+        "auxiliaries": {
+            "aux1": {
+                "connectors": {"com": "chan1"},
+                "config": None,
+                "type": "pykiso.lib.auxiliaries.dut_auxiliary:DUTAuxiliary",
+            },
+        },
+        "connectors": {
+            "chan1": {
+                "config": None,
+                "type": "pykiso.lib.connectors.cc_example:CCExample",
+            },
+        },
+    }
+
+
+@pytest.fixture
+def pykiso_config_proxy_auxiliary():
+    return {
+        "auxiliaries": {
+            "proxy_aux": {
+                "connectors": {"com": "can_channel"},
+                "config": {
+                    "aux_list": ["aux1"],
+                    "activate_trace": False,
+                    "trace_dir": "kiso-testing\\examples\\templates\\suite_proxy",
+                    "trace_name": "can_trace",
+                    "auto_start": False,
+                },
+                "type": "pykiso.lib.auxiliaries.proxy_auxiliary:ProxyAuxiliary",
+            },
+            "aux1": {
+                "connectors": {"com": "proxy_com1"},
+                "config": {"auto_start": False},
+                "type": "pykiso.lib.auxiliaries.communication_auxiliary:CommunicationAuxiliary",
+            },
+        },
+        "connectors": {
+            "proxy_com1": {
+                "config": None,
+                "type": "pykiso.lib.connectors.cc_proxy:CCProxy",
+            },
+            "can_channel": {
+                "config": {
+                    "interface": "pcan",
+                    "channel": "PCAN_USBBUS1",
+                    "state": "ACTIVE",
+                    "remote_id": 768,
+                },
+                "type": "pykiso.lib.connectors.cc_pcan_can:CCPCanCan",
+            },
+        },
+    }
+
+
+@pytest.fixture
+def runner():
+    return CliRunner()
+
+
+@pytest.mark.parametrize(
+    "value,expected", [(23, 23), ("test", '"test"'), ([233, 23], [233, 23])]
+)
+def test_format_value(value, expected):
+    assert format_value(value) == expected
+
+
+@pytest.mark.parametrize(
+    "dict_test,key_expected",
+    [
+        ({"test": "value1", "test2": "value2"}, ("test",)),
+        ({"test": {"key1": "value1"}, "test2": "value2"}, ("test", "key1")),
+        ({"test": "value", "test2": "value2"}, None),
+    ],
+)
+def test_find_string_in(dict_test, key_expected):
+
+    key = find_string_in(dict_test, "value1")
+
+    assert key == key_expected
+
+
+@pytest.mark.parametrize("copy", [True, False])
+def test_nested_get(copy):
+    dict_test = {"test": {"key1": "value1"}, "test2": "value2"}
+
+    dict_get = nested_get(dict_test, ["test"], copy=copy)
+
+    assert dict_get == dict_test["test"]
+
+
+def test_remove_key():
+    dict_test = {"test": {"key1": "value1"}, "test2": "value2"}
+
+    remove_key(dict_test, key_path=["test", "key1"])
+
+    assert dict_test["test"] == {}
+
+
+def test_get_imports(tmp_path):
+    path_file = tmp_path / "import_test.py"
+    import_to_write = [
+        "from pykiso.lib.auxiliaries.communication_auxiliary import CommunicationAuxiliary\n",
+        "from pykiso.lib.auxiliaries.proxy_auxiliary import ProxyAuxiliary\n",
+        "from pykiso.lib.connectors.cc_pcan_can import CCPCanCan\n",
+        "from pykiso.lib.connectors.cc_proxy import CCProxy\n",
+    ]
+    with open(path_file, "w", encoding="utf8") as file:
+        file.write("".join(import_to_write))
+
+    imports = get_imports(path_file)
+
+    assert imports == import_to_write
+
+
+def test_main(mocker, tmp_path, pykiso_config, runner):
+    name_file = "conf"
+    path_output = tmp_path / name_file
+    parse_config = mocker.patch(
+        "pykiso.config_parser.parse_config", return_value=pykiso_config
+    )
+
+    with runner.isolated_filesystem(temp_dir=tmp_path) as f:
+        with open("config.yaml", "w") as f:
+            pass
+        result = runner.invoke(main, ["config.yaml", f"-d{path_output}"])
+        with open(tmp_path / f"{name_file}.py", "r") as f:
+            lines = f.readlines()
+
+    parse_config.assert_called_once()
+    assert "".join(
+        [
+            "from pykiso.lib.auxiliaries.dut_auxiliary import DUTAuxiliary\n",
+            "from pykiso.lib.connectors.cc_example import CCExample\n",
+            "\n",
+            "\n",
+            '@pytest.fixture(scope="session")\n',
+            "def aux1():\n",
+            "    try:\n",
+            '        com = CCExample(name="chan1")\n',
+            "        aux1 = DUTAuxiliary(\n",
+            "            com=com,\n",
+            "        )\n",
+            "        aux1.start()\n",
+            "        aux1.create_instance()\n",
+            "    except Exception:\n",
+            '        logging.exception("something bad happened")\n',
+            "    yield aux1\n",
+            "    aux1.delete_instance()\n",
+            "    aux1.stop()\n",
+        ]
+    ) in "".join(lines)
+    assert result.exit_code == 0
+
+
+def test_main_proxy_aux(tmp_path, mocker, pykiso_config_proxy_auxiliary, runner):
+    dir_name = "conf"
+    path_output = tmp_path / dir_name
+    path_output.mkdir()
+    parse_config = mocker.patch(
+        "pykiso.config_parser.parse_config", return_value=pykiso_config_proxy_auxiliary
+    )
+
+    with runner.isolated_filesystem(temp_dir=tmp_path) as f:
+        with open("config.yaml", "w") as f:
+            pass
+        result = runner.invoke(main, ["config.yaml", f"-d{path_output}"])
+
+        with open(path_output / "conftest.py", "r") as f:
+            lines = f.readlines()
+
+    parse_config.assert_called_once()
+    assert "".join(
+        [
+            "from pykiso.lib.auxiliaries.communication_auxiliary import CommunicationAuxiliary\n",
+            "from pykiso.lib.auxiliaries.proxy_auxiliary import ProxyAuxiliary\n",
+            "from pykiso.lib.connectors.cc_pcan_can import CCPCanCan\n",
+            "from pykiso.lib.connectors.cc_proxy import CCProxy\n",
+            "\n",
+            "\n",
+            '@pytest.fixture(scope="session")\n',
+            "def aux1():\n",
+            "    try:\n",
+            '        com = CCProxy(name="proxy_com1")\n',
+            "        aux1 = CommunicationAuxiliary(\n",
+            "            com=com,\n",
+            "            auto_start=False,\n",
+            "        )\n",
+            "        aux1.start()\n",
+            "        aux1.create_instance()\n",
+            "    except Exception:\n",
+            '        logging.exception("something bad happened")\n',
+            "    yield aux1\n",
+            "    aux1.delete_instance()\n",
+            "    aux1.stop()\n",
+            "\n",
+            "\n",
+            '@pytest.fixture(scope="session")\n',
+            "def proxy(\n",
+            "    aux1,\n",
+            "):\n",
+            "    channel = CCPCanCan(\n",
+            '        name="can_channel",\n',
+            '        interface="pcan",\n',
+            '        channel="PCAN_USBBUS1",\n',
+            '        state="ACTIVE",\n',
+            "        remote_id=768,\n",
+            "    )\n",
+            "\n",
+            '    proxy_aux = ProxyAuxiliary(name="proxy_aux", com=channel, aux_list=["aux1"])\n',
+            "    proxy_aux.start()\n",
+            "    proxy_aux.create_instance()\n",
+            "    yield aux1, proxy_aux\n",
+            "    proxy_aux.delete_instance()\n",
+            "    proxy_aux.stop()\n",
+        ]
+    ) in "".join(lines)
+    assert result.exit_code == 0
--- tests/test_show_tag.py
+++ tests/test_show_tag.py
@@ -1,5 +1,5 @@
 ##########################################################################
-# Copyright (c) 2010-2022 Robert Bosch GmbH
+# Copyright (c) 2010-2023 Robert Bosch GmbH
 # This program and the accompanying materials are made available under the
 # terms of the Eclipse Public License 2.0 which is available at
 # http://www.eclipse.org/legal/epl-2.0.
@@ -7,10 +7,11 @@
 # SPDX-License-Identifier: EPL-2.0
 ##########################################################################
 
-import logging
-import unittest
+
+from unittest.mock import PropertyMock
 
 import pytest
+from click.testing import CliRunner
 
 from pykiso.tool.show_tag import *
 
@@ -64,12 +65,241 @@ def tmp_yaml_files(tmp_path: Path, request):
     return root_only, tests_dir, yamls
 
 
+@pytest.fixture
+def basic_test_mock(mocker):
+    return mocker.patch("pykiso.test_coordinator.test_case.BasicTest")
+
+
+@pytest.fixture(autouse=True, scope="module")
+def aux_mock():
+    sys.modules["pykiso.auxiliaries"] = mock.MagicMock()
+
+
+@pytest.fixture
+def runner():
+    return CliRunner()
+
+
 @pytest.mark.parametrize("tmp_yaml_files", [(False,), (True,)], indirect=True)
 def test_get_yaml_files(tmp_yaml_files):
     recurse, yaml_folder, expected_yaml_files = tmp_yaml_files
+
     yaml_files = get_yaml_files(yaml_folder, recurse)
 
     assert sorted(yaml_files) == sorted(expected_yaml_files)
+
+
+def test_get_yaml_files_file_input(mocker):
+    name_yaml = "test.yaml"
+    path_resolve_mock = mocker.patch("pathlib.Path.resolve", return_value=name_yaml)
+
+    files = get_yaml_files(name_yaml, recursive=False)
+
+    assert files == [name_yaml]
+    path_resolve_mock.assert_called_once()
+
+
+@pytest.mark.parametrize(
+    "error,return_dir", [(FileNotFoundError, True), (ValueError, False)]
+)
+def test_get_yaml_files_errors(mocker, error, return_dir):
+    path_is_dir_mock = mocker.patch("pathlib.Path.is_dir", return_value=return_dir)
+    path_glob_mock = mocker.patch("pathlib.Path.glob", return_value=[])
+
+    with pytest.raises(error):
+        get_yaml_files(".", True)
+
+    path_is_dir_mock.assert_called_once()
+    if return_dir:
+        path_glob_mock.assert_called_once_with("**/*.yaml")
+
+
+def test_get_test_cases(mocker):
+    cfg_dict = {"test_suite_list": None}
+    basic_test_suite_mock = mocker.patch(
+        "pykiso.test_coordinator.test_suite.BasicTestSuite"
+    )
+    basic_test_suite_mock._tests = ["test1", "test2"]
+    collect_test_suite = mocker.patch(
+        "pykiso.test_coordinator.test_execution.collect_test_suites",
+        return_value=[basic_test_suite_mock, basic_test_suite_mock],
+    )
+
+    test_cases = get_test_cases(cfg_dict)
+
+    assert test_cases == ["test1", "test2"] * 2
+    collect_test_suite.assert_called_once_with(cfg_dict["test_suite_list"])
+
+
+def test_get_test_cases_error():
+    with pytest.raises(ValueError):
+        get_test_cases({})
+
+
+def test_get_test_tags(basic_test_mock):
+    type(basic_test_mock).tag = PropertyMock(
+        side_effect=[
+            {"variant": ["variant1"], "branch_level": ["daily"]},
+            {"variant": ["variant1"], "branch_level": ["daily"]},
+            {"branch_level": ["nightly"], "variant": ["variant1"]},
+            {"branch_level": ["nightly"], "variant": ["variant1"]},
+            None,
+        ],
+    )
+
+    tags = get_test_tags([basic_test_mock, basic_test_mock, basic_test_mock])
+
+    assert {"variant": ["variant1"], "branch_level": ["daily", "nightly"]} == tags
+
+
+@pytest.mark.parametrize("show_test_case", [True, False])
+def test_build_result_dict(basic_test_mock, show_test_case):
+    tags = {"variant": ["variant1"], "branch_level": ["daily", "nightly"]}
+    config_file_name = "file_name"
+    len_test = 5
+
+    config_result_dict = build_result_dict(
+        config_file_name, [basic_test_mock] * len_test, tags, show_test_case
+    )
+
+    config_expected = (
+        {
+            "File name": config_file_name,
+            "Number of tests": len_test,
+            "Test cases": ("unittest.mock.MagicMock\n" * (len_test - 1))
+            + "unittest.mock.MagicMock",
+            "variant": "variant1",
+            "branch_level": "daily\nnightly",
+        }
+        if show_test_case
+        else {
+            "File name": config_file_name,
+            "Number of tests": len_test,
+            "variant": "variant1",
+            "branch_level": "daily\nnightly",
+        }
+    )
+    assert config_result_dict == config_expected
+
+
+def test_tabulate_test_information():
+    test_info = [
+        {
+            "File name": "dummy.yaml",
+            "Number of tests": 9,
+            "variant": "variant1\nvariant2\nvariant3",
+            "branch_level": "daily\nnightly",
+        }
+    ]
+
+    table_header, table_rows = tabulate_test_information(test_info)
+
+    assert table_header == ["File name", "Number of tests", "variant", "branch_level"]
+    assert table_rows == [
+        ["dummy.yaml", 9, "variant1\nvariant2\nvariant3", "daily\nnightly"]
+    ]
+
+
+@pytest.mark.parametrize("tmp_test", [("aux1", "aux2", False)], indirect=True)
+def test_show_tag_main(tmp_path, tmp_test, runner):
+    with runner.isolated_filesystem(temp_dir=tmp_path):
+        result = runner.invoke(main, [f"-c{tmp_test}"])
+
+    assert (
+        "\nAll valid configuration files have been processed successfully:"
+        in result.output
+    )
+
+    assert (
+        "╒════════════════╤═══════════════════╤═══════════╤════════════════╕\n"
+        "│ File name      │   Number of tests │ variant   │ branch_level   │\n"
+        "╞════════════════╪═══════════════════╪═══════════╪════════════════╡\n"
+        "│ aux1_aux2.yaml │                 5 │ variant1  │ daily          │\n"
+        "│                │                   │ variant2  │ nightly        │\n"
+        "╘════════════════╧═══════════════════╧═══════════╧════════════════╛\n"
+    ) in result.output
+    assert result.exit_code == 0
+
+
+@pytest.mark.parametrize(
+    "tmp_test,error_raised",
+    [
+        (("aux1", "aux2", False), ValueError("reason")),
+        (("aux1", "aux2", False), TestCollectionError("reason")),
+    ],
+    indirect=["tmp_test"],
+)
+def test_show_tag_main_fail_get_test_case(
+    tmp_path, tmp_test, error_raised, mocker, runner
+):
+    parse_config_mock = mocker.patch(
+        "pykiso.tool.show_tag.get_test_cases", side_effect=error_raised
+    )
+
+    with runner.isolated_filesystem(temp_dir=tmp_path):
+        result = runner.invoke(main, [f"-c{tmp_test}"])
+
+    assert "Failed to load test cases from config file" in result.output
+    parse_config_mock.assert_called_once()
+    assert result.exit_code == 1
+
+
+@pytest.mark.parametrize("tmp_test", [("aux1", "aux2", False)], indirect=True)
+def test_show_tag_main_fail_parse_config(tmp_path, tmp_test, mocker, runner):
+    parse_config_mock = mocker.patch("yaml.load", side_effect=ValueError())
+
+    with runner.isolated_filesystem(temp_dir=tmp_path):
+        result = runner.invoke(main, [f"-c{tmp_test}"])
+
+    assert "Failed to parse config file" in result.output
+    parse_config_mock.assert_called_once()
+    assert result.exit_code == 1
+
+
+@pytest.mark.parametrize(
+    "tmp_test,extension,function_to_patch",
+    [
+        (("aux1", "aux2", False), ".json", "json.dump"),
+        (("aux1", "aux2", False), ".csv", "csv.writer"),
+        (("aux1", "aux2", False), ".txt", "csv.writer"),
+    ],
+    indirect=["tmp_test"],
+)
+def test_show_tag_main_with_output(
+    tmp_path, tmp_test, extension, function_to_patch, mocker, runner
+):
+    output_path = tmp_path / f"result{extension}"
+    writer_mock = mocker.patch(function_to_patch)
+
+    with runner.isolated_filesystem(temp_dir=tmp_path):
+        result = runner.invoke(main, [f"-c{tmp_test}", f"-o{output_path}"])
+
+    assert f"Test configuration dumped to '{output_path}'" in result.output
+    assert result.exit_code == 0
+    if extension != ".txt":
+        writer_mock.assert_called_once()
+    else:
+        with open(output_path, "r", encoding="utf-8") as f:
+            assert f.readlines() == [
+                "╒════════════════╤═══════════════════╤═══════════╤════════════════╕\n",
+                "│ File name      │   Number of tests │ variant   │ branch_level   │\n",
+                "╞════════════════╪═══════════════════╪═══════════╪════════════════╡\n",
+                "│ aux1_aux2.yaml │                 5 │ variant1  │ daily          │\n",
+                "│                │                   │ variant2  │ nightly        │\n",
+                "╘════════════════╧═══════════════════╧═══════════╧════════════════╛",
+            ]
+
+
+@pytest.mark.parametrize("tmp_test", [("aux1", "aux2", False)], indirect=True)
+def test_show_tag_main_output_format_unsupported(tmp_path, tmp_test, runner):
+    output_suffixes = ".wrong_extension"
+    output_path = tmp_path / f"result{output_suffixes}"
+
+    with runner.isolated_filesystem(temp_dir=tmp_path):
+        result = runner.invoke(main, [f"-c{tmp_test}", f"-o{output_path}"])
+
+    assert f"Unsupported export format {output_suffixes}" in result.output
+    assert result.exit_code == 0
 
 
 @pytest.mark.parametrize("tmp_test", [("aux1", "aux2", False)], indirect=True)
@@ -102,13 +332,11 @@ def test_show_tag(tmp_test):
         ]
     ]
 
-    sys.modules["pykiso.auxiliaries"] = mock.MagicMock()
-    config_file = tmp_test
-    cfg_dict = parse_config(config_file)
+    cfg_dict = parse_config(tmp_test)
     test_cases = get_test_cases(cfg_dict)
     tags = get_test_tags(test_cases)
     single_config_result = build_result_dict(
-        config_file.name, test_cases, tags, show_test_cases=True
+        tmp_test.name, test_cases, tags, show_test_cases=True
     )
     all_results = [single_config_result]
     table_header, table_data = tabulate_test_information(all_results)
