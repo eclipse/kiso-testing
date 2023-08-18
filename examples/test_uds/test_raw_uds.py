3a347c278c87672c82d26853de859b7582fb3e92
Teddy Riolet <external.teddy.riolet@de.bosch.com>
2023-08-18 14:30:18 +0200

    chore: add comment in example

--- examples/test_uds/test_raw_uds.py
+++ examples/test_uds/test_raw_uds.py
@@ -71,7 +71,7 @@ class ExampleUdsTest(pykiso.BasicTest):
         uds_aux.start_tester_present_sender(period=5)
         time.sleep(6)
         response = uds_aux.send_uds_raw([0x10, 0x04])
-        # It is possible to check the maximum interval between to pending response messages
+        # It is possible to check the maximum interval between two pending response messages
         assert uds_aux.check_max_pending_time(response, 3)
         uds_aux.stop_tester_present_sender()
 
