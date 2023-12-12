172bf4f1fc2c4247b60e94bfc98514b3c3653fc4
Sebastian Clerson <58192998+sebclrsn@users.noreply.github.com>
2023-12-12 12:11:51 +0100

    chore: update test example

--- examples/test_suite_1/test_suite_1.py
+++ examples/test_suite_1/test_suite_1.py
@@ -79,7 +79,6 @@ class MyTest1(pykiso.BasicTest):
     -> test_ids: [optional] store the requirements into the report
     -> tag: [optional] allows the run of subset of tests
     """
-    properties = {"testrail_attachment": "some/path/to/afile.txt"}
 
     @pykiso.retry_test_case(max_try=3)
     def setUp(self):
@@ -102,6 +101,9 @@ class MyTest1(pykiso.BasicTest):
         logging.info(
             f"--------------- RUN: {self.test_suite_id}, {self.test_case_id} ---------------"
         )
+        # define any additional key-value pair that will appear as property in the JUnit report
+        properties = {"testrail_attachment": "some/path/to/afile.txt"}
+        
         self.assertTrue(next(side_effect))
         logging.info(f"I HAVE RUN 0.1.1 for tag {self.tag}!")
 
