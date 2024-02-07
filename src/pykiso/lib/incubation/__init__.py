ef42a6a3c25b0146fb898873fcf35a6f7fd85f2b
sebastianpfischer <sebastian.a.fischer@accenture.com>
2024-02-07 15:30:34 +0100

    docs: introduce api lifecycle

---
+++ src/pykiso/lib/incubation/__init__.py
@@ -0,0 +1,18 @@
+##########################################################################
+# Copyright (c) 2023-2024 Accenture GmbH
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0.
+#
+# SPDX-License-Identifier: EPL-2.0
+##########################################################################
+
+"""
+Incubation
+**********
+
+:module: incubation
+
+.. currentmodule:: incubation
+
+"""
